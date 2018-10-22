bcd3a1a48cc5b7b544659d204a528fda8a004328
Max <cubeitadmin@protonmail.com>
2018-10-22 08:06:47 +0200

    Working login/register system

--- Code/Login_Code/app/src/main/java/cubeit/cubeit_login_test/MainActivity.kt
+++ Code/Login_Code/app/src/main/java/cubeit/cubeit_login_test/MainActivity.kt
@@ -1,50 +1,96 @@
 package cubeit.cubeit_login_test
 
-import android.app.Application
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import io.realm.ObjectServerError
 import io.realm.Realm
 import io.realm.SyncCredentials
-import kotlinx.android.synthetic.main.activity_main.*
-import android.R.attr.password
-import android.widget.Toast
-import io.realm.ObjectServerError
 import io.realm.SyncUser
-import io.realm.permissions.UserCondition.username
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 public class MainActivity : AppCompatActivity()  {
+
+    val authURL: String = "https://cubeit-test.de1a.cloud.realm.io/auth" //static URL of Realm server (In final build this should be in a seperate file)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Realm.init(this) // Initialzes Realm
 
-
-        val authURL: String = "https://cubeit-test.de1a.cloud.realm.io/" //static URL of Realm server (In final build this should be in a seperate file)
-
-        var userEmail: String = ""
-        var userPassword: String = "" //Creds taken from login screen
-
-        button.setOnClickListener {
-            userEmail = editTextMail.text.toString()
-            userPassword = editTextPassword.text.toString()
-        }
-
-        var credentials: SyncCredentials = SyncCredentials.usernamePassword(userEmail, userPassword)
-
-        SyncUser.loginAsync(credentials, authURL, object:SyncUser.Callback<SyncUser> { // !Unresolved reference error!
-            override fun onSuccess(user:SyncUser) {
-                // User is ready
-                // Can also be accessed using `SyncUser.currentUser()` if only one
-                // user is logged in.
-            }
-            override fun onError(error:ObjectServerError) {
-                // Something went wrong
-            }
-        })
-
+        button.setOnClickListener({ view -> userLogin() })
+        button4.setOnClickListener { view -> userCreate() }
 
     }
+
+    fun userCreate() {
+
+        val userEmail = editTextName.text.toString()
+        val userPassword = editTextPassword.text.toString()
+
+        val credentials: SyncCredentials = SyncCredentials.usernamePassword(userEmail, userPassword, true)
+
+        SyncUser.logInAsync(credentials, authURL, object:SyncUser.Callback<SyncUser> {
+            override fun onSuccess(user: SyncUser) {
+
+                val builder = AlertDialog.Builder(this@MainActivity)
+
+                builder.setTitle("Success")
+                builder.setMessage("User created!")
+
+                val dialog: AlertDialog = builder.create()
+
+                dialog.show()
+            }
+
+            override fun onError(error: ObjectServerError) {
+
+                val builder = AlertDialog.Builder(this@MainActivity)
+
+                builder.setTitle("Fail")
+                builder.setMessage("User already exists!")
+
+                val dialog: AlertDialog = builder.create()
+
+                dialog.show()
+            }
+        })
+    }
+
+
+    fun userLogin() {
+
+        val userEmail = editTextName.text.toString()
+        val userPassword = editTextPassword.text.toString()
+
+        val credentials: SyncCredentials = SyncCredentials.usernamePassword(userEmail, userPassword, false)
+
+        SyncUser.logInAsync(credentials, authURL, object:SyncUser.Callback<SyncUser> {
+            override fun onSuccess(user:SyncUser) {
+
+                val builder = AlertDialog.Builder(this@MainActivity)
+
+                builder.setTitle("Success")
+                builder.setMessage("User logged in!")
+
+                val dialog: AlertDialog = builder.create()
+
+                dialog.show()
+            }
+            override fun onError(error:ObjectServerError) {
+
+                val builder = AlertDialog.Builder(this@MainActivity)
+
+                builder.setTitle("Fail")
+                builder.setMessage("Email/Fassword incorrect!")
+
+                val dialog: AlertDialog = builder.create()
+
+                dialog.show()
+            }
+        })
+    }
+
 }
