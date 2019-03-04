0633bddf99465e4de6089a3b3c6914d41cc6a5af
Jacob CubeIt <39309757+CodeNameCubeIt@users.noreply.github.com>
2019-03-04 14:03:49 +0100

    An actually working saving of item method

---
+++ Code/CubeIt/app/src/main/java/cz/cubeit/cubeit/Home.kt
@@ -0,0 +1,163 @@
+package cz.cubeit.cubeit
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+import android.os.IBinder
+import android.app.Service
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+//import android.arch.lifecycle.ProcessLifecycleOwner
+import android.content.Context
+import android.view.Window
+
+
+var music = true
+var songs = arrayOf(
+        Song(R.raw.song1, "Shrek"),
+        Song(R.raw.song2, "Country roads"),
+        Song(R.raw.song3, "Kero Kero Bonito - Flamingo"),
+        Song(R.raw.song4, "Bite your soul"),
+        Song(R.raw.song5, "Népal - Fugu"),
+        Song(R.raw.song6, "Lonepsi - freestyle"),
+        Song(R.raw.song7, "Bohemian Rhapsody"),
+        Song(R.raw.song8, "Kudasai")
+        )
+var playedSong = R.raw.song1
+
+class Song(songRawIn:Int = R.raw.song1, descriptionIn:String = "Shrek"){
+    val songRaw = songRawIn
+    val description = descriptionIn
+}
+
+class SampleLifecycleListener(val context: Context) : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onMoveToForeground() {
+        context.stopService(Intent(context, CubeItHeadService::class.java))
+        if(!BackgroundSoundService().mediaPlayer.isPlaying&& music){
+            val svc = Intent(context, BackgroundSoundService(playedSong)::class.java)
+            context.startService(svc)
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onMoveToBackground() {
+        context.startService(Intent(context, CubeItHeadService::class.java))
+        val svc = Intent(context, BackgroundSoundService(playedSong)::class.java)
+        context.stopService(svc)
+        player.uploadPlayer()
+    }
+}
+
+class BackgroundSoundService(private val raw:Int = R.raw.song2) : Service() {
+    var mediaPlayer = MediaPlayer()
+    override fun onBind(arg0: Intent): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer.create(this, playedSong)
+        mediaPlayer.isLooping = true // Set looping
+        mediaPlayer.setVolume(100f, 100f)
+
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        mediaPlayer.start()
+        return START_STICKY
+    }
+
+    override fun onStart(intent: Intent, startId: Int) {
+    }
+
+    fun onPause() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+
+    fun restart(){
+        mediaPlayer = MediaPlayer.create(this, playedSong)
+    }
+
+    override fun onDestroy() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+
+    override fun onLowMemory() {
+    }
+}
+
+
+class Home : AppCompatActivity() {
+
+    private val lifecycleListener: SampleLifecycleListener by lazy{
+        SampleLifecycleListener(this)
+    }
+
+//    private fun setupLifecycleListener() {
+//        ProcessLifecycleOwner.get().lifecycle.addObserver(lifecycleListener) TODO lifecycle
+//    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(Intent(this, CubeItHeadService::class.java))
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        if(!BackgroundSoundService().mediaPlayer.isPlaying&& music){
+            val svc = Intent(this, BackgroundSoundService(playedSong)::class.java)
+            startService(svc)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+//        setupLifecycleListener()
+
+
+        Hatch.setOnClickListener{
+            val intent = Intent(this, FightSystem::class.java)
+            startActivity(intent)
+            this.overridePendingTransition(0,0)
+        }
+        Skills.setOnClickListener{
+            val intent = Intent(this, Spells()::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            this.overridePendingTransition(0,0)
+            player.uploadPlayer()                                           //---------------
+        }
+        Character.setOnClickListener{
+            val intent = Intent(this, cz.cubeit.cubeit.Character::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            this.overridePendingTransition(0,0)
+        }
+        SettingsHome.setOnClickListener{
+            val intent = Intent(this, cz.cubeit.cubeit.Settings::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            this.overridePendingTransition(0,0)
+        }
+        Shop.setOnClickListener {
+            val intent = Intent(this, cz.cubeit.cubeit.Shop::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            this.overridePendingTransition(0,0)
+        }
+        Adventure.setOnClickListener{
+            val intent = Intent(this, cz.cubeit.cubeit.Adventure::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            this.overridePendingTransition(0,0)
+        }
+    }
+}
