38bc2b4c297a7b730210dc9cb3216ad1dff0e50a
Jacob CubeIt <39309757+CodeNameCubeIt@users.noreply.github.com>
2019-03-05 13:24:15 +0100

    Background changes 3. vystup_03
    Login/register is dependent on player MexxFM

---
+++ Code/CubeIt/app/src/main/java/cz/cubeit/cubeit/FragmentAdventureSecond.kt
@@ -0,0 +1,21 @@
+package cz.cubeit.cubeit
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.second_fragment_adventure.view.*
+
+class FragmentAdventureSecond : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view:View = inflater.inflate(R.layout.second_fragment_adventure, container, false)
+        val opts = BitmapFactory.Options()
+        opts.inScaled = false
+        val bitmap = BitmapFactory.decodeResource(resources, surfaces[1].background, opts)
+        view.surface2.setImageBitmap(bitmap)
+        return view
+    }
+}
