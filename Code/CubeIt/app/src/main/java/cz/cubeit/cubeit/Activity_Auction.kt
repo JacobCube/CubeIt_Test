8aa1d1032722acad54a131877c4fed296fad7222
Max <cubeitadmin@protonmail.com>
2019-03-11 23:51:21 +0100

    I swear to god if this doesn't work I will literally kill myself.
    No joke

---
+++ Code/CubeIt/app/src/main/java/cz/cubeit/cubeit/Activity_Auction.kt
@@ -0,0 +1,25 @@
+package cz.cubeit.cubeit
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import java.text.DateFormat
+import java.util.*
+
+class Auction:AppCompatActivity(){
+
+    //val Offers: HashMap<String, MutableList<Offer>> = hashMapOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+class Offer(inItem:Item, inUntilDate: DateFormat, inPrice:Int, inPlayer: Player){
+    val item = inItem
+    val untilDate = inUntilDate                     //untilDate.calendar.add(Calendar.DAY_OF_YEAR, days)
+    val price = inPrice
+    val registeredBy = inPlayer
+
+    fun addOffer(){
+
+    }
+}
